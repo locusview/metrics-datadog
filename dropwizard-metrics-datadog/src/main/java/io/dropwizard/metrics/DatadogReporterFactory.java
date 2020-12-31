4af01fbee1fd873b3b6e136aba7786b91d6ab7dd
Ori Vider <ori.vider@locusview.com>
2020-12-31 19:30:47 +0200

    Removed pom files Changed packaging of dropwizard-metrics-datadog to be a fat jar

--- dropwizard-metrics-datadog/src/main/java/io/dropwizard/metrics/DatadogReporterFactory.java
+++ dropwizard-metrics-datadog/src/main/java/io/dropwizard/metrics/DatadogReporterFactory.java
@@ -12,6 +12,7 @@ import org.coursera.metrics.datadog.transport.AbstractTransportFactory;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
+import java.io.IOException;
 import java.util.EnumSet;
 import java.util.List;
 
@@ -20,46 +21,60 @@ import static org.coursera.metrics.datadog.DatadogReporter.Expansion;
 @JsonTypeName("datadog")
 public class DatadogReporterFactory extends BaseReporterFactory {
 
-  @JsonProperty
-  private String host = null;
+    @JsonProperty
+    private String host = null;
 
-  @JsonProperty
-  private List<String> tags = null;
+    @JsonProperty
+    private List<String> tags = null;
 
-  @Valid
-  @JsonProperty
-  private DynamicTagsCallbackFactory dynamicTagsCallback = null;
+    @Valid
+    @JsonProperty
+    private DynamicTagsCallbackFactory dynamicTagsCallback = null;
 
-  @JsonProperty
-  private String prefix = null;
+    @JsonProperty
+    private String prefix = null;
 
-  @Valid
-  @NotNull
-  @JsonProperty
-  private EnumSet<Expansion> expansions = EnumSet.allOf(Expansion.class);
+    @JsonProperty
+    private boolean useEc2Host;
 
-  @Valid
-  @NotNull
-  @JsonProperty
-  private MetricNameFormatterFactory metricNameFormatter = new DefaultMetricNameFormatterFactory();
+    @Valid
+    @NotNull
+    @JsonProperty
+    private EnumSet<Expansion> expansions = EnumSet.allOf(Expansion.class);
 
-  @Valid
-  @NotNull
-  @JsonProperty
-  private AbstractTransportFactory transport = null;
+    @Valid
+    @NotNull
+    @JsonProperty
+    private MetricNameFormatterFactory metricNameFormatter = new DefaultMetricNameFormatterFactory();
 
-  public ScheduledReporter build(MetricRegistry registry) {
-    return DatadogReporter.forRegistry(registry)
-        .withTransport(transport.build())
-        .withHost(host)
-        .withTags(tags)
-        .withPrefix(prefix)
-        .withExpansions(expansions)
-        .withMetricNameFormatter(metricNameFormatter.build())
-        .withDynamicTagCallback(dynamicTagsCallback != null ? dynamicTagsCallback.build() : null)
-        .filter(getFilter())
-        .convertDurationsTo(getDurationUnit())
-        .convertRatesTo(getRateUnit())
-        .build();
+    @Valid
+    @NotNull
+    @JsonProperty
+    private AbstractTransportFactory transport = null;
+
+    public ScheduledReporter build(MetricRegistry registry) {
+        try {
+            final DatadogReporter.Builder ddReporterBuilder = DatadogReporter.forRegistry(registry);
+            ddReporterBuilder
+                    .withTransport(transport.build())
+                    .withTags(tags)
+                    .withPrefix(prefix)
+                    .withExpansions(expansions)
+                    .withMetricNameFormatter(metricNameFormatter.build())
+                    .withDynamicTagCallback(dynamicTagsCallback != null ? dynamicTagsCallback.build() : null)
+                    .filter(getFilter())
+                    .convertDurationsTo(getDurationUnit())
+                    .convertRatesTo(getRateUnit());
+
+            if (useEc2Host) {
+                ddReporterBuilder.withEC2Host();
+            } else {
+                ddReporterBuilder.withHost(host);
+            }
+
+            return ddReporterBuilder.build();
+        } catch (IOException e) {
+            throw new RuntimeException("Error while trying to build DD ScheduledReporter", e);
+        }
     }
 }
